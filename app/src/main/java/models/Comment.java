e53afbd18c7c6ba60c4564570a8af8c38d958670
jdmauthe <mauthejd@gmail.com>
2020-02-14 22:53:29 -0800

    added Comment model

---
+++ app/src/main/java/models/Comment.java
@@ -0,0 +1,42 @@
+package models;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Comment {
+
+    @SerializedName("id")
+    @Expose
+    private int id;
+    @SerializedName("username")
+    @Expose
+    private String username;
+    @SerializedName("text")
+    @Expose
+    private String text;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+}
