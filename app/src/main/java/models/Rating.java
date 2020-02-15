cb2e255d3ca2367e945b59886c1de6fa88d47edf
jdmauthe <mauthejd@gmail.com>
2020-02-14 22:55:05 -0800

    added Rating model

---
+++ app/src/main/java/models/Rating.java
@@ -0,0 +1,42 @@
+package models;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Rating {
+
+    @SerializedName("id")
+    @Expose
+    private int id;
+    @SerializedName("username")
+    @Expose
+    private String username;
+    @SerializedName("score")
+    @Expose
+    private int score;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+}
