e56a6ee8a666b8766e5063119454b98d16832e6c
jdmauthe <mauthejd@gmail.com>
2020-02-15 22:30:36 -0800

    added invintation view functionality

---
+++ app/src/main/java/adapters/InvitationRecyclerViewAdapter.java
@@ -0,0 +1,120 @@
+package adapters;
+
+        import android.content.Context;
+        import android.graphics.Color;
+        import android.view.LayoutInflater;
+        import android.view.View;
+        import android.view.ViewGroup;
+        import android.widget.TextView;
+
+        import androidx.annotation.NonNull;
+        import androidx.recyclerview.widget.RecyclerView;
+
+        import com.example.notehub.R;
+        import com.google.android.material.button.MaterialButton;
+
+        import java.util.List;
+
+        import de.hdodenhof.circleimageview.CircleImageView;
+        import models.Invitation;
+
+// Invitation Recycler View Adapter
+public class InvitationRecyclerViewAdapter extends RecyclerView.Adapter<InvitationRecyclerViewAdapter.ViewHolder> {
+    private Context context;
+    private List<Invitation> invitations;
+    private onItemClickListener listener;
+
+    // Invitation Constructor
+    public InvitationRecyclerViewAdapter(Context context, List<Invitation> invitations ) {
+        this.context = context;
+        this.invitations = invitations;
+    }
+
+    public interface onItemClickListener {
+        void onClickButton(int position);
+        void onItemClick(int position);
+        void onDeleteClick();
+    }
+
+    public void setOnItemClickListener(onItemClickListener listener) { this.listener = listener; }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view;
+        view = LayoutInflater.from(context).inflate(R.layout.invitation_card_view, parent, false);
+        ViewHolder viewHolder = new ViewHolder(view, listener);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.groupName.setText(invitations.get(position).getGroupName());
+        holder.username.setText(invitations.get(position).getUsername());
+        // holder.image.setImageResource(invitations.get(position).getPhoto());
+    }
+
+    @Override
+    public int getItemCount() {
+        return this.invitations.size();
+    }
+
+    public void addItem(Invitation invitation) {
+        if (invitation != null) {
+            invitations.add(invitation);
+            notifyItemInserted(invitations.size() - 1);
+        }
+    }
+
+    public void removeItem(int position) {
+        if (invitations.size() != 0)
+            invitations.remove(position);
+        notifyItemRemoved(position);
+    }
+
+    // Invitation View Holder
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private TextView groupName;
+        private TextView username;
+        private MaterialButton button;
+        private CircleImageView image;
+
+
+        // View Constructor
+        public ViewHolder(@NonNull View itemView, final onItemClickListener listener) {
+            super(itemView);
+
+            groupName = itemView.findViewById(R.id.invitation_group_name_text);
+            username = itemView.findViewById(R.id.invitation_username_text);
+            button = itemView.findViewById(R.id.invitation_button);
+            button.setText("Join");
+            button.setBackgroundColor(0xffff4444);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (listener != null) {
+                        int position = getAdapterPosition();
+
+                        if (position != RecyclerView.NO_POSITION) {
+                            listener.onItemClick(position);
+                        }
+                    }
+                }
+            });
+
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (listener != null) {
+                        int position = getAdapterPosition();
+
+                        if (position != RecyclerView.NO_POSITION) {
+                            listener.onClickButton(position);
+                        }
+                    }
+                }
+            });
+        }
+    }
+}
--- app/src/main/java/fragments/GroupsNotificationFragment.java
+++ app/src/main/java/fragments/GroupsNotificationFragment.java
@@ -1,6 +1,146 @@
 package fragments;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.notehub.GroupActivity;
+import com.example.notehub.LoginActivity;
+import com.example.notehub.MainActivity;
+import com.example.notehub.NoteActivity;
+import com.example.notehub.R;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import adapters.InvitationRecyclerViewAdapter;
+import models.Invitation;
+import models.Membership;
+import remote.ApiInterface;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class GroupsNotificationFragment extends Fragment {
+    View view;
+    private RecyclerView recyclerView;
+    InvitationRecyclerViewAdapter recyclerViewAdapter;
+    private List<Invitation> invitations;
+    private ApiInterface apiService;
+    private EditText groupNameEdit;
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.groups_notification_fragment, container, false);
+        apiService = MainActivity.buildHTTP();
+
+        createInvitationList();
+        return view;
+    }
+
+    // Load invitation list
+    public void createInvitationList() {
+        invitations = new ArrayList<>();
+
+        Call<List<Invitation>> call = apiService.getUserInvitations(MainActivity.getToken(getContext()));
+        buildRecyclerView();
+        call.enqueue(new Callback<List<Invitation>>() {
+            @Override
+            public void onResponse(Call<List<Invitation>> call, Response<List<Invitation>> response) {
+                // Success; store invitations
+                if (response.errorBody() == null) {
+                    //invitations = response.body();
+                    for (Invitation invitation : response.body())
+                        recyclerViewAdapter.addItem(invitation);
+                }
+                else
+                    showAlertMessage("Error, could not load users invitations.", "Ok");
+
+            }
+
+            @Override
+            public void onFailure(Call<List<Invitation>> call, Throwable t) {
+            }
+        });
+    }
+
+    // Initialize recycler
+    public void buildRecyclerView() {
+        recyclerView = view.findViewById(R.id.groups_notification_recycler);
+
+        // Defines the list displaying in recycler view and set layout to linear
+        recyclerViewAdapter = new InvitationRecyclerViewAdapter(getContext(), invitations);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        // Set recycler view to use custom comment adapter
+        recyclerView.setAdapter(recyclerViewAdapter);
+
+        recyclerViewAdapter.setOnItemClickListener(new InvitationRecyclerViewAdapter.onItemClickListener() {
+
+            @Override
+            public void onClickButton(final int position) {
+                Membership membership = new Membership();
+                Call<Membership> call = apiService.uploadGroupMembership(MainActivity.getToken(getContext()), invitations.get(position).getGroup(), membership);
+                call.enqueue(new Callback<Membership>() {
+
+                    @Override
+                    public void onResponse(Call<Membership> call, Response<Membership> response) {
+                        if (response.errorBody() == null) {
+                            recyclerViewAdapter.removeItem(position);
+                            Membership data = response.body();
+                            showAlertMessage("You successfully joined " + data.getGroupName() + "!", "Ok");
+                        } else {
+
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<Membership> call, Throwable t) {
+
+                    }
+                });
+            }
+
+            @Override
+            public void onItemClick(int position) {
+
+            }
+
+            @Override
+            public void onDeleteClick() {
+
+            }
+        });
+    }
+
+    private void showAlertMessage(String message, String buttonText) {
+        new MaterialAlertDialogBuilder(getActivity())
+                .setMessage(message)
+                .setPositiveButton(buttonText, null)
+                .show();
+    }
 }
